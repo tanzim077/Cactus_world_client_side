28c7c856b4e05257c144c699c045199ae0a929b4
tanzim077 <tanzimahmed1994@gmail.com>
2021-11-13 20:51:59 +0600

    Banner style updated

--- src/pages/HomePage/TopBanner/TopBanner.js
+++ src/pages/HomePage/TopBanner/TopBanner.js
@@ -2,94 +2,56 @@ import React from 'react';
 import './TopBanner.css';
 import { Link } from 'react-router-dom';
 import bg1 from '../../../images/bg1.jpg'
+import Typed from 'typed.js';
 
 const TopBanner = () => {
+    const el = React.useRef(null);
+    const typed = React.useRef(null);
 
-    var TxtType = function (el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function () {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-        }
-
-            setTimeout(function () {
-                that.tick();
-            }, delta);
+    React.useEffect(() => {
+        const options = {
+            strings: [
+                'World <i>of</i> Cactus',
+                'Customize<strong> your</strong> home'
+            ],
+            typeSpeed: 50,
+            backSpeed: 50,
         };
 
-        window.onload = function () {
-            var elements = document.getElementsByClassName('typewrite');
-            for (var i = 0; i < elements.length; i++) {
-                var toRotate = elements[i].getAttribute('data-type');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtType(elements[i], JSON.parse(toRotate), period);
-                }
-            }
-        //     // INJECT CSS
-            var css = document.createElement("style");
-            css.type = "text/css";
-            css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid transparent}";
-            document.body.appendChild(css);
-        };
+       typed.current = new Typed(el.current, options);
 
-        return (
-            <div className="container-fluid">
-                <div className="static-slider7" style={{
-                    backgroundImage: `url(${bg1})`
-                }}>
-                    <div className="container">
+        return () => {
+            typed.current.destroy();
+        }
+    }, [])
 
-                        <div className="row justify-content-center">
+    return (
+        <div className="container-fluid">
+            <div className="static-slider7" style={{
+                backgroundImage: `url(${bg1})`
+            }}>
+                <div className="container">
 
-                            <div className="col-md-8 align-self-center text-center">
-                                <h1 className="title text-white typewrite" data-period="2000" data-type='[ "World",
-                            "Of",  "Cactus" ]'></h1>
-                                <h5 className="text-white">Best Home Decorated Cactus Planet Suppliers</h5>
-                                <Link to='/items'>
-                                    <a className="btn btn-danger-gradiant rounded-pill btn-md mt-3 border-0 text-white" href="">
-                                        <span>Our Collections <i className="ti-arrow-right"></i></span>
-                                    </a>
-                                </Link>
-                                <Link to='/aboutus'>
-                                    <a className="btn btn-outline-light rounded-pill btn-md mt-3" data-toggle="modal" data-target="#static-slide7" href=""><i className="mr-2 icon-control-play"></i> About US </a>
-                                </Link>
-                            </div>
+                    <div className="row justify-content-center">
 
+                        <div className="col-md-8 align-self-center text-center">
+                            <h1 className="title text-white"><span style={{ whiteSpace: 'pre' }} ref={el} /></h1>
+                            <h5 className="text-white">Best Home Decorated Cactus Planet Suppliers</h5>
+                            <Link to='/items'>
+                                <a className="btn btn-danger-gradiant rounded-pill btn-md mt-3 border-0 text-white" href="">
+                                    <span>Our Collections <i className="ti-arrow-right"></i></span>
+                                </a>
+                            </Link>
+                            <Link to='/aboutus'>
+                                <a className="btn btn-outline-light rounded-pill btn-md mt-3" data-toggle="modal" data-target="#static-slide7" href=""><i className="mr-2 icon-control-play"></i> About US </a>
+                            </Link>
                         </div>
+
                     </div>
                 </div>
             </div>
-        );
-    };
+        </div>
+    );
+};
 
-    export default TopBanner;
+export default TopBanner;
